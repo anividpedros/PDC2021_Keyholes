d5024a8e14bd31e99096b41ec834b65f36f6449e
Anivid <anividpedros@gmail.com>
2021-03-12 20:03:44 -0700

    last edits

--- MOIDtest.m
+++ MOIDtest.m
@@ -4,8 +4,23 @@ clear
 close all
 format shortG
 
-filename = 'main_1flyby_pdc2021.m';
+filename = 'MOIDtest.m';
 filepath = matlab.desktop.editor.getActiveFilename;
 currPath = filepath(1:(end-length(filename)));
 cd(currPath)
 addpath(genpath(pwd))
+
+
+A.sma = 1.00000011;
+A.e = 0.01671022;
+A.argp = deg2rad(102.94719 );
+A.Omega = deg2rad(-11.26064);
+A.i = deg2rad(0.00005);
+
+B.sma = 1.5;
+B.e = 0.3;
+B.argp = deg2rad(140);
+B.Omega = deg2rad(-35);
+B.i = deg2rad(10);
+
+[moid] = ComputeMOID(A,B)
